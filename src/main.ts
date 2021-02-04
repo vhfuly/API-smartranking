98ab644581924ca5e555b5390561139c30c31d5a
vhfuly <victor_hugo_fuly@hotmail.com>
2021-02-04 17:08:13 -0300

    feat: add date-fns

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionFilter } from './common/filters/http-exception.filter';
+import { format } from 'date-fns-tz'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new AllExceptionFilter)
   await app.listen(3000);
+
+  Date.prototype.toJSON = function(): any {
+    return format(this, 'yyyy-MM-dd HH:mm:ss.SSS', { timeZone: 'America/Sao_Paulo' })
+  }
+
 }
 bootstrap();
